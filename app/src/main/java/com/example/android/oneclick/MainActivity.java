83af4840b88f27c5e040b6b959c61164e5f999a6
Suprit <suprit98@gmail.com>
2018-04-07 12:25:06 -0700

    Base layout

--- app/src/main/java/com/example/android/oneclick/MainActivity.java
+++ app/src/main/java/com/example/android/oneclick/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.oneclick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView healthView = (TextView) findViewById(R.id.health);
+        healthView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent healthIntent = new Intent(MainActivity.this,HealthActivity.class);
+                startActivity(healthIntent);
+
+            }
+        });
     }
 }
